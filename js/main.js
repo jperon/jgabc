1ed7aa8a45b19663ce72ba9a07503c91fd02b557
jacques <cataclop@hotmail.com>
2014-02-19 16:52:54 +0100

    Perfectionnement des boutons.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,22 @@ function ecrirenoteryhtme(){
 	chiffre_lettrine.firstChild.nodeValue=document.getElementById("prependedInput").value;
 }
 
+function ecrireediteur(e){
+        var startPos = document.getElementById("editor").selectionStart;
+        var endPos = document.getElementById("editor").selectionEnd;
+        document.getElementById("editor").value = document.getElementById("editor").value.substring(0, startPos)
+            + e
+            + document.getElementById("editor").value.substring(endPos, document.getElementById("editor").value.length);
+		$('#editor').keyup();
+        document.getElementById('editor').focus();
+        var place = startPos + e.length;
+        document.getElementById('editor').setSelectionRange(place, place);
+}
+
 function ecrirepartition(e){
+    if($("#twoBoxes").is(':hidden')){
+        ecrireediteur(e);
+    } else {
         var startPos = document.getElementById("hymngabc").selectionStart;
         var endPos = document.getElementById("hymngabc").selectionEnd;
         document.getElementById("hymngabc").value = document.getElementById("hymngabc").value.substring(0, startPos)
@@ -17,9 +32,13 @@ function ecrirepartition(e){
         document.getElementById('hymngabc').focus();
         var place = startPos + e.length;
         document.getElementById('hymngabc').setSelectionRange(place, place);
+    }
 }
 
 function ecriretexte(e){
+    if($("#twoBoxes").is(':hidden')){
+        ecrireediteur(e);
+    } else{
         var startPos = document.getElementById("hymntext").selectionStart;
         var endPos = document.getElementById("hymntext").selectionEnd;
         document.getElementById("hymntext").value = document.getElementById("hymntext").value.substring(0, startPos)
@@ -29,6 +48,7 @@ function ecriretexte(e){
         document.getElementById('hymntext').focus();
         var place = startPos + e.length;
         document.getElementById('hymntext').setSelectionRange(place, place);
+    }
 }
 
 function visible(){
